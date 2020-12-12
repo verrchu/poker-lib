b0ac6720adab60e1b35041dbc762e37a2d8672f0
Evgeniy Terekhin <e_terekhin@wargaming.net>
2020-12-13 00:24:11 +0200

    [0.1.2] add constructor for Card

--- src/card/mod.rs
+++ src/card/mod.rs
@@ -7,3 +7,24 @@ use std::hash::Hash;
 
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 pub struct Card(pub(crate) Rank, pub(crate) Suit);
+
+impl Card {
+    pub fn new(rank: Rank, suit: Suit) -> Self {
+        Card(rank, suit)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Card;
+    use super::Rank;
+    use super::Suit;
+
+    #[test]
+    fn test_new() {
+        assert_eq!(
+            Card::new(Rank::Two, Suit::Diamonds),
+            Card(Rank::Two, Suit::Diamonds)
+        );
+    }
+}
