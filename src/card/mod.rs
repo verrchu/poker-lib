3db29b504b18077c2d623db07a2af3b13fa0a99f
Evgeniy Terekhin <e_terekhin@wargaming.net>
2020-12-13 00:51:01 +0200

    [0.1.4] impl Ord for Card

--- src/card/mod.rs
+++ src/card/mod.rs
@@ -3,6 +3,7 @@ pub use rank::Rank;
 pub mod suit;
 pub use suit::Suit;
 
+use std::cmp::Ordering;
 use std::hash::Hash;
 
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
@@ -22,8 +23,25 @@ impl Card {
     }
 }
 
+impl PartialOrd for Card {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.rank().partial_cmp(&other.rank())
+    }
+}
+
+impl Ord for Card {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.rank().cmp(&other.rank())
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use ::claim::*;
+    use ::itertools::Itertools;
+
+    use std::cmp::Ordering;
+
     use super::Card;
     use super::Rank;
     use super::Suit;
@@ -44,5 +62,41 @@ mod tests {
     #[test]
     fn test_get_suit() {
         assert_eq!(Card::new(Rank::Two, Suit::Diamonds).suit(), Suit::Diamonds);
+    }
+
+    #[test]
+    fn test_ordering() {
+        assert_eq!(
+            Card(Rank::Two, Suit::Diamonds).cmp(&Card(Rank::Two, Suit::Diamonds)),
+            Ordering::Equal
+        );
+        assert_eq!(
+            Card(Rank::Two, Suit::Diamonds).cmp(&Card(Rank::Two, Suit::Hearts)),
+            Ordering::Equal
+        );
+        assert_eq!(
+            Card(Rank::Two, Suit::Diamonds).cmp(&Card(Rank::Three, Suit::Hearts)),
+            Ordering::Less
+        );
+        assert_eq!(
+            Card(Rank::Three, Suit::Diamonds).cmp(&Card(Rank::Two, Suit::Hearts)),
+            Ordering::Greater
+        );
+
+        assert_eq!(
+            vec![
+                Card(Rank::Four, Suit::Diamonds),
+                Card(Rank::Two, Suit::Hearts),
+                Card(Rank::Four, Suit::Clubs)
+            ]
+            .into_iter()
+            .sorted()
+            .collect::<Vec<_>>(),
+            vec![
+                Card(Rank::Two, Suit::Hearts),
+                Card(Rank::Four, Suit::Diamonds),
+                Card(Rank::Four, Suit::Clubs)
+            ]
+        );
     }
 }
