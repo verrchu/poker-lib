21f5671a0168c211deb8231691fac6dfc3ca358c
Evgeniy Terekhin <e_terekhin@wargaming.net>
2020-12-13 11:55:08 +0200

    [0.1.5] change module privacy

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
-pub mod card;
+mod card;
 pub use card::Card;
 pub use card::Rank;
 pub use card::Suit;
 
-pub mod combination;
+mod combination;
 pub use combination::Combination;
 
-pub mod game;
+mod game;
 pub use game::Board;
+pub use game::Game;
 pub use game::HandOf2;
 pub use game::HandOf4;
 pub use game::HandOf5;
